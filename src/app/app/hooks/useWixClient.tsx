dee086b7ab32816a7de178fa21a8e707b6187b9c
Priya22Raj <rajpriya4439@gmail.com>
2025-07-20 18:11:10 +0530

    commit

---
+++ src/app/app/hooks/useWixClient.tsx
@@ -0,0 +1,9 @@
+"use client"
+
+import { WixClientContext } from "../context/WixContext";
+import { useContext } from "react";
+
+export const useWixClient = ( ) => {
+    return
+    useContext(WixClientContext);
+}
